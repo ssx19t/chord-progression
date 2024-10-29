ee051da17619186a5bbde3d9ad66dc223e28767d
ssx19t <105140098+ssx19t@users.noreply.github.com>
2024-10-29 13:50:47 +0900

    Update similarity.js

--- similarity.js
+++ similarity.js
@@ -1,155 +1,58 @@
 function myFunction() {
+  const NOTE_MAP = { 'C': 0, 'D': 2, 'E': 4, 'F': 5, 'G': 7, 'A': 9, 'B': 11 };
+  
   const parseNum = (onmei, i) => {
-    let num = 0;
-    switch (onmei.charAt(0)) {
-      case 'C': num = 0; break;
-      case 'D': num = 2; break;
-      case 'E': num = 4; break;
-      case 'F': num = 5; break;
-      case 'G': num = 7; break;
-      case 'A': num = 9; break;
-      case 'B': num = 11; break;
-      default : num = null;
-    }
-    num += i;
-    if (onmei.includes('#')) {
-      num += 1;
-    }
-    if (onmei.includes('b')) {
-      num -= 1;
-    }
-  num = num % 12;
-  return num;
-  }
-  const transpose = (chord, i) => {
+    let num = NOTE_MAP[onmei.charAt(0)] ?? null;
+    if (num === null) return null;
 
+    num += i + (onmei.includes('#') ? 1 : 0) - (onmei.includes('b') ? 1 : 0);
+    return num % 12;
   }
+
   const parseTriad = (chordName) => {
-    let result = chordName.replace('add9', '');
-    result = chordName.replace('M9', '');
-    result = chordName.replace('+9', '');
-    result = chordName.replace('M7', '');
-    result = chordName.replace('7', '');
-    result = chordName.replace('69', '');
-    result = chordName.replace('9', '');
-    result = chordName.replace('dim', 'm-5');
-    return result;
+    return chordName.replace(/(add9|M9|\+9|M7|7|69|9|dim)/g, match => (match === 'dim' ? 'm-5' : ''));
   }
+
+  const PATTERN_MAP = {
+    'sus2': [0, 2, 7],
+    'sus4': [0, 5, 7],
+    'aug': [0, 4, 8],
+    'm-5': [0, 3, 6],
+    '-5': [0, 4, 6],
+    'm': [0, 3, 7],
+    'default': [0, 4, 7],
+  };
+
   const kouseion = (chord) => {
     const chordName = parseTriad(chord);
-    // それぞれの音が何回含まれるか配列でカウントする
-    let count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    if (chordName.includes('/')) {
-      // 分数コードの場合
-      const root = chordName.split('/')[1];
-      count[parseNum(root, 0)] += 1;
-      const bunshi = parseTriad(chordName.split('/')[0]);
+    let count = Array(12).fill(0);
 
-      if (bunshi.includes('sus2')) {
-        count[parseNum(bunshi.replace('sus2'), 0)] += 1;
-        count[parseNum(bunshi.replace('sus2'), 2)] += 1;
-        count[parseNum(bunshi.replace('sus2'), 7)] += 1;
+    const [baseChord, root] = chordName.split('/');
+    if (root) count[parseNum(root, 0)] += 1;
 
-      } else if (bunshi.includes('sus4')) {
-        count[parseNum(bunshi.replace('sus4'), 0)] += 1;
-        count[parseNum(bunshi.replace('sus4'), 5)] += 1;
-        count[parseNum(bunshi.replace('sus4'), 7)] += 1;
+    const pattern = Object.keys(PATTERN_MAP).find(key => baseChord.includes(key)) || 'default';
+    PATTERN_MAP[pattern].forEach(interval => {
+      count[parseNum(baseChord.replace(pattern, ''), interval)] += pattern === 'default' ? 2 : 1;
+    });
 
-      } else if (bunshi.includes('aug')) {
-        count[parseNum(bunshi.replace('aug'), 0)] += 1;
-        count[parseNum(bunshi.replace('aug'), 4)] += 1;
-        count[parseNum(bunshi.replace('aug'), 8)] += 1;
-
-      } else if (bunshi.includes('m-5')) {
-        count[parseNum(bunshi.replace('m-5'), 0)] += 1;
-        count[parseNum(bunshi.replace('m-5'), 3)] += 1;
-        count[parseNum(bunshi.replace('m-5'), 6)] += 1;
-
-      } else if (bunshi.includes('-5')) {
-        count[parseNum(bunshi.replace('-5'), 0)] += 1;
-        count[parseNum(bunshi.replace('-5'), 4)] += 1;
-        count[parseNum(bunshi.replace('-5'), 6)] += 1;
-
-      } else if (bunshi.includes('m')) {
-        count[parseNum(bunshi.replace('m'), 0)] += 1;
-        count[parseNum(bunshi.replace('m'), 3)] += 1;
-        count[parseNum(bunshi.replace('m'), 7)] += 1;
-
-      } else {
-        count[parseNum(bunshi, 0)] += 1;
-        count[parseNum(bunshi, 4)] += 1;
-        count[parseNum(bunshi, 7)] += 1;
-
-      }
-    } else {
-      // 分数コード以外の場合
-      const bunshi = chordName;
-
-      if (bunshi.includes('sus2')) {
-        count[parseNum(bunshi.replace('sus2'), 0)] += 2;
-        count[parseNum(bunshi.replace('sus2'), 2)] += 1;
-        count[parseNum(bunshi.replace('sus2'), 7)] += 1;
-
-      } else if (bunshi.includes('sus4')) {
-        count[parseNum(bunshi.replace('sus4'), 0)] += 2;
-        count[parseNum(bunshi.replace('sus4'), 5)] += 1;
-        count[parseNum(bunshi.replace('sus4'), 7)] += 1;
-
-      } else if (bunshi.includes('aug')) {
-        count[parseNum(bunshi.replace('aug'), 0)] += 2;
-        count[parseNum(bunshi.replace('aug'), 4)] += 1;
-        count[parseNum(bunshi.replace('aug'), 8)] += 1;
-
-      } else if (bunshi.includes('m-5')) {
-        count[parseNum(bunshi.replace('m-5'), 0)] += 2;
-        count[parseNum(bunshi.replace('m-5'), 3)] += 1;
-        count[parseNum(bunshi.replace('m-5'), 6)] += 1;
-
-      } else if (bunshi.includes('-5')) {
-        count[parseNum(bunshi.replace('-5'), 0)] += 2;
-        count[parseNum(bunshi.replace('-5'), 4)] += 1;
-        count[parseNum(bunshi.replace('-5'), 6)] += 1;
-
-      } else if (bunshi.includes('m')) {
-        count[parseNum(bunshi.replace('m'), 0)] += 2;
-        count[parseNum(bunshi.replace('m'), 3)] += 1;
-        count[parseNum(bunshi.replace('m'), 7)] += 1;
-
-      } else {
-        count[parseNum(bunshi, 0)] += 2;
-        count[parseNum(bunshi, 4)] += 1;
-        count[parseNum(bunshi, 7)] += 1;
-
-      }
-    }
     return count;
   }
+
   const chordSimilarity = (chord1, chord2) => {
     const kousei1 = kouseion(chord1);
     const kousei2 = kouseion(chord2);
-    let sum = 0;
-    for (let i = 0; i < 12; i++) {
-      sum += (kousei1[i] - kousei2 [i]) ** 2;
-    }
-    return 1 - Math.sqrt(sum / 12);
+    return 1 - Math.sqrt(kousei1.reduce((sum, count, i) => sum + (count - kousei2[i]) ** 2, 0) / 12);
   }
+
   const chordProgressionSimilarity = (a, b) => {
-    let chordP1 = a;
-    let chordP2 = b;
-    let result = 0;
-    for (let i = 0; i < 8; i++) {
-      if (chordP1[i] == '') {
-        chordP1[i] = chordP1[i - 1]
-      }
-      if (chordP2[i] == '') {
-        chordP2[i] = chordP2[i - 1]
-      }
-      result += chordSimilarity(chordP1[i], chordP2[i]);
-    }
-    return result / 8;
+    const filledChordP1 = a.map((chord, i) => chord || a[i - 1]);
+    const filledChordP2 = b.map((chord, i) => chord || b[i - 1]);
+    
+    return filledChordP1.reduce((sum, chord, i) => sum + chordSimilarity(chord, filledChordP2[i]), 0) / 8;
   }
+
   console.log(chordSimilarity('Am', 'Cm/Eb'));
-  const chordP1 = ['C/E','F' , 'G','G#dim' , 'Am', 'G', 'F','Fm/G'];
-  const chordP2 = ['C', '' ,'G', 'G#dim', 'Am', '' , 'Gm', 'C'];
-  console.log('[' + chordP1 + '] と\n[' + chordP2 + '] の\nコード進行の類似度は\n' + chordProgressionSimilarity(chordP1, chordP2))
+  const chordP1 = ['C/E', 'F', 'G', 'G#dim', 'Am', 'G', 'F', 'Fm/G'];
+  const chordP2 = ['C', '', 'G', 'G#dim', 'Am', '', 'Gm', 'C'];
+  console.log('[' + chordP1 + '] と\n[' + chordP2 + '] の\nコード進行の類似度は\n' + chordProgressionSimilarity(chordP1, chordP2));
 }
